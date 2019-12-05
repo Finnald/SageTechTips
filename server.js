7be64b39971fcd97837cf80c08dbfadba9f561a7
AidanVicars <aidanvicars@gmail.com>
2019-12-05 21:03:00 +0800

    added code to server.js and package.json

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const port = process.env.PORT || 3000
+
+
+app.use(express.static(path.join(__dirname + "/public/")));
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
