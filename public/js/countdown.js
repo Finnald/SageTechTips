52eea1efb653a5b3854d154deffaf09520636fac
Finnald <copsnrobbers95@gmail.com>
2019-12-05 20:06:16 +0800

    bullet cool

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -7,7 +7,7 @@ let x = setInterval(function() {
   let now = new Date().getTime();
   let distance = countDownDate - now;
 
-  //time calculations
+  //quick maths
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
